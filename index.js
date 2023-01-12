0d7af22fb23fd2087c77d685419b074902832af7
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-01-12 17:58:37 -0500

    adding inquirer questions to program

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
 const inquirer = require('inquirer');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
-console.log("Am I Running?");
+// App questions
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?'
+    }, 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project description?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'installation?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Project usage?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'contribution?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'email?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'GitHub?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'license?'
+        choices: ['MIT', 'ISC', 'GNUPLv3'],
+        filter(val) {
+            return val.toLowerCase();
+        }
+    }
+];
+
+// Run async query function 
+
+async function runQuery() {
+    return inquirer.prompt(questions)
+    .then((answers) =>{
+        console.log(answers);
+        return answers;
+    })
+}
